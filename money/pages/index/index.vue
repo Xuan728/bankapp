857df77956202e910bd1c9c2629988ad8bf0e1d3
Xuan728 <101514936+Xuan728@users.noreply.github.com>
2023-11-16 22:35:17 +0800

    1

--- money/pages/index/index.vue
+++ money/pages/index/index.vue
@@ -2,6 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
+<<<<<<< HEAD
 			<text style="font-size: 40rpx;">{{title}}</text>
 		</view>
 		<view class="uni-form-item" style="margin-top: 200rpx;">
@@ -12,11 +13,15 @@
 			<view class="title">密码</view>
 			<input class="uni-input" @input="inputpassword" password type="text" placeholder="请输入密码" />
 			<view class="uni-button"></view>
+=======
+			<text class="title">{{title}}</text>
+>>>>>>> 10a6fe85fd93c517e8b3790412e5ff4666c9e9b7
 		</view>
 	</view>
 </template>
 
 <script>
+<<<<<<< HEAD
 	import axios from "../../node_modules/axios"
 	export default {
 		data() {
@@ -24,12 +29,19 @@
 				title: '用户您好!',
 				mobile:"",
 				password:"",
+=======
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+>>>>>>> 10a6fe85fd93c517e8b3790412e5ff4666c9e9b7
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+<<<<<<< HEAD
 			 inputmobile: function(event) {
 			            this.mobile = event.target.value
 			        },
@@ -42,6 +54,11 @@
 	axios.post("http://localhost:8081/user/login",mobile,password).then((res) => {
 	        console.log(res);
 	})
+=======
+
+		}
+	}
+>>>>>>> 10a6fe85fd93c517e8b3790412e5ff4666c9e9b7
 </script>
 
 <style>
@@ -69,6 +86,7 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+<<<<<<< HEAD
 		width: 20%;
 	}
 		
@@ -82,5 +100,7 @@
 		margin: 0 0 0 60rpx;
 		background-color: aliceblue;
 		padding: 3rpx 30rpx;
+=======
+>>>>>>> 10a6fe85fd93c517e8b3790412e5ff4666c9e9b7
 	}
 </style>
