18363505044f33b8f2d4da519f9ca22acc168c23
CoreAttacker <97329851+CoreAttacker@users.noreply.github.com>
2023-11-23 21:00:07 +0800

    实体类

---
+++ test/springboot-mybatis/src/main/java/com/itheima/pojo/贷款产品.java
@@ -0,0 +1,14 @@
+package com.example.app.pojo;
+
+public class 贷款产品 {
+
+    public 贷款产品() {
+    }
+
+    public String 贷款产品名称;
+
+    public double 利率;
+
+    public double 额度;
+
+}
---
+++ test/springboot-mybatis/src/main/java/com/itheima/pojo/银行卡.java
@@ -0,0 +1,21 @@
+package com.example.app.pojo;
+
+public class 银行卡 {
+
+    public 银行卡() {
+    }
+
+    public String 银行卡号;
+
+    public double 余额;
+
+    public String 交易情况;
+
+    public void 修改余额() {
+        // TODO implement here
+    }
+    public void 查询余额() {
+        // TODO implement here
+    }
+
+}
